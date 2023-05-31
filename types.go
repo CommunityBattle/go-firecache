fb16546303fc8b695e895c1f4f938100c59ceabb
Dainismo <manuel99stein@gmail.com>
2023-05-31 23:10:10 +0200

    added order and limit to query

--- types.go
+++ types.go
@@ -1,9 +1,18 @@
 package firecache
 
+type Order struct {
+	by        string
+	direction string
+}
+
+type O []Order
+
 type Query struct {
 	field    string
 	operator string
 	value    string
+	order    O
+	limit    int
 }
 
 type Q []Query
